df0ea1653cb9bfce1d75dd161e6194f038b45c41
Dmitry Marov <d.marov94@gmail.com>
2019-04-02 13:21:52 +0300

    update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ then
 
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3status
-mkdir -p ~/.config/i3status
+mkdir -p ~/.config/i3blocks
 
 ln -sf $PWD/i3.conf ~/.config/i3/config
 ln -sf $PWD/i3status.conf ~/.config/i3status/config
