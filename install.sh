0dd5ab75ef224fa8a416ed0d1afc527e698face3
Dmitry Marov <d.marov94@gmail.com>
2019-04-13 20:47:14 +0300

    update

--- install.sh
+++ install.sh
@@ -12,3 +12,17 @@ ln -sf $PWD/i3status.conf ~/.config/i3status/config
 ln -sf $PWD/i3blocks.conf ~/.config/i3blocks/config
 
 fi;
+
+
+if [ $(uname) == 'Linux' ];
+then
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+mkdir -p ~/.config/i3blocks
+
+ln -sf $PWD/i3.conf ~/.config/i3/config
+ln -sf $PWD/i3status.conf ~/.config/i3status/config
+ln -sf $PWD/i3blocks.conf ~/.config/i3blocks/config
+
+fi;
