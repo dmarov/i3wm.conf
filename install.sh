454d4f48826263eefdfd8b375ec23744c51231ef
Dmitry Marov <d.marov94@gmail.com>
2022-09-23 22:08:14 +0300

    added startup

--- install.sh
+++ install.sh
@@ -23,5 +23,6 @@ mkdir -p ~/.config/i3blocks
 ln -sf $PWD/i3.conf ~/.config/i3/config
 ln -sf $PWD/i3status.conf ~/.config/i3status/config
 ln -sf $PWD/i3blocks.conf ~/.config/i3blocks/config
+ln -sf $PWD/polybar.launch.sh ~/.config/polybar/launch.sh
 
 fi;
