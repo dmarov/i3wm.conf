cb8f44f851eb5af8db50836cb722fcd99c3fa275
Dmitry Marov <d.marov94@gmail.com>
2023-02-08 13:24:06 +0300

    updated configs

--- polybar.launch.sh
+++ polybar.launch.sh
@@ -6,9 +6,5 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-MONITOR=HDMI-1 polybar monitor1 2>&1 | tee -a /tmp/polybar1.log & disown
-MONITOR=HDMI-2 polybar monitor1 2>&1 | tee -a /tmp/polybar2.log & disown
-
-echo "Bars launched..."
+MONITOR=HDMI-1 polybar monitor1 & disown
+MONITOR=HDMI-2 polybar monitor1 & disown
