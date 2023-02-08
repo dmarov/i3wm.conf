40b7b657611dede3d96265bb101037a59de8a890
Dmitry Marov <d.marov94@gmail.com>
2023-02-08 13:18:16 +0300

    resolved conflicts

--- install.sh
+++ install.sh
@@ -1,26 +1,7 @@
 #!/bin/sh 
 
-if [ $(uname) == 'FreeBSD' ];
-then
-
 mkdir -p ~/.config/i3
-mkdir -p ~/.config/i3status
-mkdir -p ~/.config/i3blocks
-
-ln -sf $PWD/i3.conf ~/.config/i3/config
-ln -sf $PWD/i3status.conf ~/.config/i3status/config
-ln -sf $PWD/i3blocks.conf ~/.config/i3blocks/config
-
-fi;
-
-if [ $(uname) == 'Linux' ];
-then
-
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/polybar
 
 ln -sf $PWD/i3.conf ~/.config/i3/config
 ln -sf $PWD/polybar.config.ini ~/.config/polybar/config.ini
 ln -sf $PWD/polybar.launch.sh ~/.config/polybar/launch.sh
-
-fi;
